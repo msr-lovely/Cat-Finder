ab755c011bd7b4f9413c9c0d5d4bf3507b4996e4
MelimeliDev <melisa.surja.pro@gmail.com>
2020-12-17 21:22:15 +0800

    Error Page

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ For detailed explanation on how things work, check out [Nuxt.js docs](https://nu
 - [x] ~~Landing Page Parallax~~
 - [ ] Landing Page Scroll Reveal Animation
 - [ ] Landing Page Text Fix
-- [ ] Error Page
+- [x] ~~Error Page~~
 
 
 ## Credits
---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex items-center justify-center fixed inset-0 text-center" style="z-index:-1">
+      <div>
+        <img src="images/Error.svg" class="md:w-full w-3/4 mb-8 max-w-lg mx-auto" />
+        <h1 class="md:text-5xl text-4xl font-bold text-gray-600">
+            <template v-if="error.statusCode === 404">
+                Page not found
+            </template>
+            <template v-else>An error occurred</template>
+        </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    // layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
